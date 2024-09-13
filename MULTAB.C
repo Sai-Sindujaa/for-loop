2c020d85e7b8ea5ed65c206b7cbc561440c8142b
Sai-Sindujaa <sindujaa3399@gmail.com>
2024-09-13 12:10:12 +0530

    Add files via upload

---
+++ MULTAB.C
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,i,j;
+clrscr();
+printf("enter a value :");
+scanf("%d",&n);
+for(i=1;i<=n;i++)
+{
+for(j=1;j<=10;j++)
+{
+printf("%d\t",(i*j));
+}
+printf("\n");
+}
+getch();
+}
